6009573c39c50f0f43da3e03a03d2af526fb75c7
Mrunzzz <mrunmayiggt@gmail.com>
2017-06-07 13:32:46 +0530

    Map getLocation and setBounds

--- app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragments/MapFragment.java
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragments/MapFragment.java
@@ -1,7 +1,9 @@
 package in.ac.iitb.gymkhana.iitbapp.fragments;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import in.ac.iitb.gymkhana.iitbapp.R;
@@ -34,6 +37,20 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED){
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        }
+
+
+        LatLngBounds iitbBounds=new LatLngBounds(new LatLng(19.1249000, 72.9046000),new LatLng(19.143522, 72.920000));
+        googleMap.setLatLngBoundsForCameraTarget(iitbBounds);
+        googleMap.setMaxZoomPreference(30);
+        googleMap.setMinZoomPreference(18);
         // Position the map's camera near Mumbai
         LatLng iitb = new LatLng(19.1334, 72.9133);
         googleMap.addMarker(new MarkerOptions().position(iitb)
