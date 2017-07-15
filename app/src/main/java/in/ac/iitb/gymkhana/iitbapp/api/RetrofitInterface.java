894a2a302e2bb22a65ddb84e3444a3b233b9121b
Mrunzzz <mrunmayiggt@gmail.com>
2017-07-15 15:31:10 +0530

    consume createEvent api

--- app/src/main/java/in/ac/iitb/gymkhana/iitbapp/api/RetrofitInterface.java
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/api/RetrofitInterface.java
@@ -1,5 +1,7 @@
 package in.ac.iitb.gymkhana.iitbapp.api;
 
+import in.ac.iitb.gymkhana.iitbapp.api.model.EventCreateRequest;
+import in.ac.iitb.gymkhana.iitbapp.api.model.EventCreateResponse;
 import in.ac.iitb.gymkhana.iitbapp.api.model.LoginRequest;
 import in.ac.iitb.gymkhana.iitbapp.api.model.LoginResponse;
 import retrofit2.Call;
@@ -9,4 +11,7 @@ import retrofit2.http.POST;
 public interface RetrofitInterface {
     @POST("/login/")
     Call<LoginResponse> login(@Body LoginRequest loginRequest);
+
+    @POST("/createEvent/")
+    Call<EventCreateResponse> eventCreate(@Body EventCreateRequest eventCreateRequest);
 }
--- app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragment/AddEventFragment.java
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragment/AddEventFragment.java
@@ -2,8 +2,10 @@ package in.ac.iitb.gymkhana.iitbapp.fragment;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +19,24 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.sql.Timestamp;
 import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import in.ac.iitb.gymkhana.iitbapp.MainActivity;
 import in.ac.iitb.gymkhana.iitbapp.R;
+import in.ac.iitb.gymkhana.iitbapp.api.RetrofitInterface;
+import in.ac.iitb.gymkhana.iitbapp.api.ServiceGenerator;
+import in.ac.iitb.gymkhana.iitbapp.api.model.EventCreateRequest;
+import in.ac.iitb.gymkhana.iitbapp.api.model.EventCreateResponse;
+import in.ac.iitb.gymkhana.iitbapp.api.model.LoginRequest;
+import in.ac.iitb.gymkhana.iitbapp.api.model.LoginResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.ContentValues.TAG;
 
 
 public class AddEventFragment extends Fragment {
@@ -31,6 +46,8 @@ public class AddEventFragment extends Fragment {
     CheckBox publicCheckBox;
     @BindView(R.id.tv_date)
     TextView date;
+    @BindView(R.id.et_eventName)
+    EditText eventName;
     @BindView(R.id.tv_time)
     TextView time;
     @BindView(R.id.et_venue)
@@ -41,6 +58,8 @@ public class AddEventFragment extends Fragment {
     ImageView eventImage;
     @BindView(R.id.ib_eventImage)
     ImageButton imageButton;
+    Timestamp timestamp;
+    int publicStatus;
     View view;
 
 
@@ -55,62 +74,86 @@ public class AddEventFragment extends Fragment {
         container.removeAllViews();
 
         view = inflater.inflate(R.layout.fragment_add_event, container, false);
-        ButterKnife.bind(this,view);
+        ButterKnife.bind(this, view);
 
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar calendar=Calendar.getInstance();
-                int mYear=calendar.get(Calendar.YEAR);
-                int mMonth=calendar.get(Calendar.MONTH);
-                int mDay=calendar.get(Calendar.DAY_OF_MONTH);
-                
-                DatePickerDialog datePickerDialog=new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
-                  
+                Calendar calendar = Calendar.getInstance();
+                int mYear = calendar.get(Calendar.YEAR);
+                int mMonth = calendar.get(Calendar.MONTH);
+                int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+                long millis = calendar.getTimeInMillis();
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         date.setText(dayOfMonth + "/" + month + "/" + year);
                     }
-                } ,mYear,mMonth,mDay);
-            datePickerDialog.show();
+                }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+                timestamp = new Timestamp(millis);
             }
 
         });
+        if (publicCheckBox.isChecked()) {
+            publicStatus = 1;
+        } else
+            publicStatus = 0;
 
         time.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Calendar calendar=Calendar.getInstance();
-                int mHour=calendar.get(Calendar.HOUR_OF_DAY);
-                int mMin=calendar.get(Calendar.MINUTE);
-
-                TimePickerDialog timePickerDialog=new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                Calendar calendar = Calendar.getInstance();
+                int mHour = calendar.get(Calendar.HOUR_OF_DAY);
+                int mMin = calendar.get(Calendar.MINUTE);
+                long millis = calendar.getTimeInMillis();
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        time.setText(hourOfDay+":"+minute);
+                        time.setText(hourOfDay + ":" + minute);
                     }
-                },mHour,mMin,true);
+                }, mHour, mMin, true);
                 timePickerDialog.show();
+                timestamp = new Timestamp(millis);
 
             }
         });
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"Add Image", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Add Image", Toast.LENGTH_SHORT).show();
                 //TODO (1) upload image to server
             }
         });
         createEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"Add Event", Toast.LENGTH_SHORT).show();
-                //TODO (2) save event
+                addEvent();
             }
         });
+
+
         return view;
     }
-    //TODO(3) configure onBackPressed
+
+    public void addEvent() {
+        EventCreateRequest eventCreateRequest = new EventCreateRequest(eventName.getText().toString(), details.getText().toString(), venue.getText().toString(), timestamp, timestamp, publicStatus, 0, 0);
+        RetrofitInterface retrofitInterface = ServiceGenerator.createService(RetrofitInterface.class);
+        retrofitInterface.eventCreate(eventCreateRequest).enqueue(new Callback<EventCreateResponse>() {
+            @Override
+            public void onResponse(Call<EventCreateResponse> call, Response<EventCreateResponse> response) {
+                Toast.makeText(getContext(), "Event Created", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<EventCreateResponse> call, Throwable t) {
+                Toast.makeText(getContext(), "Event Creation Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 
 }
