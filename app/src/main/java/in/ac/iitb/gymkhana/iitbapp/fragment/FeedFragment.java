e90b89c4e7e95a0e92a7cbe5e5310a8c2fded5ef
yvsriram <yvsriram99@gmail.com>
2017-07-16 19:51:23 +0530

    Resolved merge conflict

--- app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragment/FeedFragment.java
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragment/FeedFragment.java
@@ -5,6 +5,8 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,9 +14,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import in.ac.iitb.gymkhana.iitbapp.Constants;
 import in.ac.iitb.gymkhana.iitbapp.ItemClickListener;
 import in.ac.iitb.gymkhana.iitbapp.R;
 import in.ac.iitb.gymkhana.iitbapp.adapter.FeedAdapter;
@@ -52,7 +57,7 @@ public class FeedFragment extends Fragment {
         super.onStart();
         Cursor cursor = getContext().getContentResolver().query(DatabaseContract.NewsFeedEntry.CONTENT_URI, null, null, null, null);
         if (cursor.getCount() != 0) {
-            List<Event> events = new ArrayList<>();
+            final List<Event> events = new ArrayList<>();
             while (cursor.moveToNext()) {
                 Event event = new Event(cursor.getString(cursor.getColumnIndex(DatabaseContract.NewsFeedEntry.COLUMN_EVENT_NAME)),
                         cursor.getString(cursor.getColumnIndex(DatabaseContract.NewsFeedEntry.COLUMN_EVENT_DESCRIPTION)),
@@ -65,7 +70,15 @@ public class FeedFragment extends Fragment {
             FeedAdapter feedAdapter = new FeedAdapter(events, new ItemClickListener() {
                 @Override
                 public void onItemClick(View v, int position) {
-                    // TODO: Launch EventFragment
+                    String eventJson = new Gson().toJson(events.get(position));
+                    Bundle bundle = new Bundle();
+                    bundle.putString(Constants.EVENT_JSON, eventJson);
+                    EventFragment eventFragment = new EventFragment();
+                    eventFragment.setArguments(bundle);
+                    FragmentManager manager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    transaction.replace(R.id.framelayout_for_fragment, eventFragment, eventFragment.getTag());
+                    transaction.commit();
                 }
             });
             feedRecyclerView = (RecyclerView) getActivity().findViewById(R.id.feed_recycler_view);
@@ -81,12 +94,20 @@ public class FeedFragment extends Fragment {
             public void onResponse(Call<NewsFeedResponse> call, Response<NewsFeedResponse> response) {
                 if (response.isSuccessful()) {
                     NewsFeedResponse newsFeedResponse = response.body();
-                    List<Event> events = newsFeedResponse.getEvents();
+                    final List<Event> events = newsFeedResponse.getEvents();
 
                     FeedAdapter feedAdapter = new FeedAdapter(events, new ItemClickListener() {
                         @Override
                         public void onItemClick(View v, int position) {
-                            // TODO: Launch EventFragment
+                            String eventJson = new Gson().toJson(events.get(position));
+                            Bundle bundle = new Bundle();
+                            bundle.putString(Constants.EVENT_JSON, eventJson);
+                            EventFragment eventFragment = new EventFragment();
+                            eventFragment.setArguments(bundle);
+                            FragmentManager manager = getActivity().getSupportFragmentManager();
+                            FragmentTransaction transaction = manager.beginTransaction();
+                            transaction.replace(R.id.framelayout_for_fragment, eventFragment, eventFragment.getTag());
+                            transaction.commit();
                         }
                     });
                     feedRecyclerView = (RecyclerView) getActivity().findViewById(R.id.feed_recycler_view);
