280d3750cca13a22cf75a52073acfe6ab3997257
Sajal Narang <sajalnarang@gmail.com>
2018-03-24 02:32:36 +0530

    Implement Login API, fix #45

--- app/src/main/java/in/ac/iitb/gymkhana/iitbapp/api/RetrofitInterface.java
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/api/RetrofitInterface.java
@@ -4,7 +4,6 @@ import in.ac.iitb.gymkhana.iitbapp.api.model.EventCreateRequest;
 import in.ac.iitb.gymkhana.iitbapp.api.model.EventCreateResponse;
 import in.ac.iitb.gymkhana.iitbapp.api.model.ImageUploadRequest;
 import in.ac.iitb.gymkhana.iitbapp.api.model.ImageUploadResponse;
-import in.ac.iitb.gymkhana.iitbapp.api.model.LoginRequest;
 import in.ac.iitb.gymkhana.iitbapp.api.model.LoginResponse;
 import in.ac.iitb.gymkhana.iitbapp.api.model.NewsFeedResponse;
 import in.ac.iitb.gymkhana.iitbapp.api.model.NotificationsRequest;
@@ -17,8 +16,8 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RetrofitInterface {
-    @POST("login/")
-    Call<LoginResponse> login(@Body LoginRequest loginRequest);
+    @GET("login")
+    Call<LoginResponse> login(@Query("code") String AUTH_CODE, @Query("redir") String redirectURI, @Query("fcm_id") String fcmID);
 
     @POST("events")
     Call<EventCreateResponse> createEvent(@Body EventCreateRequest eventCreateRequest);
