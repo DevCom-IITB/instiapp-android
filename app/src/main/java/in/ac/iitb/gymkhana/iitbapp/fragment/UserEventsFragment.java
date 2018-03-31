bc5ff99c5647776bbbf861376cc350f883583b07
Owais Chunawala <chunawalaowais@gmail.com>
2018-04-01 02:15:21 +0530

    added tabs in profile fragment

---
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragment/UserEventsFragment.java
@@ -0,0 +1,37 @@
+package in.ac.iitb.gymkhana.iitbapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.ac.iitb.gymkhana.iitbapp.R;
+
+public class UserEventsFragment extends Fragment {
+
+
+
+
+    public UserEventsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_events, container, false);
+    }
+
+}
---
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/fragment/UserFollowingFragment.java
@@ -0,0 +1,37 @@
+package in.ac.iitb.gymkhana.iitbapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.ac.iitb.gymkhana.iitbapp.R;
+
+
+public class UserFollowingFragment extends Fragment {
+
+
+    public UserFollowingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_following, container, false);
+    }
+
+
+
+}
