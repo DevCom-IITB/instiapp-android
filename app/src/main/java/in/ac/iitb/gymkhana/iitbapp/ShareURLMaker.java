32615123a90cb2f7479fd47705893fe91dfc71d3
Varun Patil <radialapps@gmail.com>
2018-04-03 22:36:08 +0530

    Add external class for making shared URLs with new URL scheme

---
+++ app/src/main/java/in/ac/iitb/gymkhana/iitbapp/ShareURLMaker.java
@@ -0,0 +1,15 @@
+package in.ac.iitb.gymkhana.iitbapp;
+
+import in.ac.iitb.gymkhana.iitbapp.data.Event;
+
+/**
+ * Created by varun on 03-Apr-18.
+ */
+
+public final class ShareURLMaker {
+    public static final String WEB_HOST = "https://instiapp.wncc-iitb.org/";
+
+    public static final String getEventURL(Event event) {
+        return WEB_HOST + "event/" + event.getEventID();
+    }
+}
