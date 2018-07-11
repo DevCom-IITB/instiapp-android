c4a4c6fc791d8ff18738848ee3f94cd69ccf26d9
Varun Patil <radialapps@gmail.com>
2018-07-11 19:38:07 +0530

    Close keyboard on search

--- app/src/main/java/app/insti/fragment/MapFragment.java
+++ app/src/main/java/app/insti/fragment/MapFragment.java
@@ -1,5 +1,6 @@
 package app.insti.fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Color;
@@ -34,6 +35,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -919,7 +921,14 @@ public class MapFragment extends Fragment implements TextWatcher,
     }
 
     private void hideKeyboard() {
-
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = getActivity().getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(getActivity());
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     @Override
