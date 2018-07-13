dee031814b9b5523492f77a433d0e021a168a82c
Varun Patil <radialapps@gmail.com>
2018-07-13 12:17:55 +0530

    Check if user hostel is null before opening mess menu

--- app/src/main/java/app/insti/MainActivity.java
+++ app/src/main/java/app/insti/MainActivity.java
@@ -275,7 +275,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         bundle.putString(Constants.SESSION_ID, session.pref.getString(Constants.SESSION_ID, "Error"));
         if (fragment instanceof MessMenuFragment)
-            bundle.putString(Constants.USER_HOSTEL, session.isLoggedIn() ? currentUser.getHostel() : "1");
+            bundle.putString(Constants.USER_HOSTEL, session.isLoggedIn() && currentUser.getHostel() != null ? currentUser.getHostel() : "1");
         if (fragment instanceof SettingsFragment && session.isLoggedIn())
             bundle.putString(Constants.USER_ID, currentUser.getUserID());
         fragment.setArguments(bundle);
