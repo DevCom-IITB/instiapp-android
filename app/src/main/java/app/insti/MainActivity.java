3d294b15eace1d3168b9f47c3e31b09630978c53
Varun Patil <radialapps@gmail.com>
2018-07-30 18:28:21 +0530

    Enable SSL checks for Picasso

--- app/src/main/java/app/insti/MainActivity.java
+++ app/src/main/java/app/insti/MainActivity.java
@@ -46,6 +46,7 @@ import app.insti.fragment.QuickLinksFragment;
 import app.insti.fragment.SettingsFragment;
 import app.insti.fragment.TrainingBlogFragment;
 import app.insti.notifications.NotificationEventReceiver;
+import okhttp3.OkHttpClient;
 
 import static app.insti.Constants.MY_PERMISSIONS_REQUEST_ACCESS_LOCATION;
 import static app.insti.Constants.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE;
@@ -348,7 +349,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void initPicasso() {
         Picasso.Builder builder = new Picasso.Builder(getApplicationContext());
         builder.downloader(new com.squareup.picasso.OkHttp3Downloader((
-                UnsafeOkHttpClient.getUnsafeOkHttpClient(getApplicationContext())
+                new OkHttpClient.Builder().build()
         )));
         Picasso built = builder.build();
         built.setIndicatorsEnabled(false);
