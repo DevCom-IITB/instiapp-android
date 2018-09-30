f04e6406d6559108d13f33025da392d720144bf4
Varun Patil <radialapps@gmail.com>
2018-09-30 22:22:59 +0530

    Move creation of singleton Picasso to Application onCreate This allows using cached images for notifications

--- app/src/main/java/app/insti/InstiAppFirebaseMessagingService.java
+++ app/src/main/java/app/insti/InstiAppFirebaseMessagingService.java
@@ -13,7 +13,6 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
