b514005a2a0bfdf7b62e9c416ac052ae0ffb8e35
sshivam95 <sshivam95@gmail.com>
2018-10-02 17:09:32 +0530

    Venter: update TagClass.java

--- app/src/main/java/app/insti/TagClass.java
+++ app/src/main/java/app/insti/TagClass.java
@@ -1,20 +1,11 @@
 package app.insti;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.support.v4.content.ContextCompat;
-
-import java.util.ArrayList;
-import java.util.Random;
-
 /**
  * Created by Shivam Sharma on 13-08-2018.
  */
 public class TagClass {
 
     private String name;
-    private String color;
 
     public TagClass(String name) {
         this.name = name;
