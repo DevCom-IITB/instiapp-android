5a474cf535cf319ba5475dc18972e2d3daaacb03
Sajal <sajalnarang@gmail.com>
2018-10-10 01:56:14 +0530

    Remove unused imports

--- app/src/main/java/app/insti/api/model/NewsArticle.java
+++ app/src/main/java/app/insti/api/model/NewsArticle.java
@@ -8,7 +8,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.sql.Timestamp;
 
-import app.insti.Utils;
 import app.insti.interfaces.Clickable;
 
 import static app.insti.Utils.openWebURL;
