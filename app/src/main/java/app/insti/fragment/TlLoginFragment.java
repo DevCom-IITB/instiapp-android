99315c449f1664cf89faed290ae9576feb1a9709
e-aakash <09e.aakash@gmail.com>
2018-11-01 21:43:31 +0530

    Added TL Login Fragment

---
+++ app/src/main/java/app/insti/fragment/TlLoginFragment.java
@@ -0,0 +1,22 @@
+package app.insti.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.insti.R;
+
+
+public class TlLoginFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tl_login, container, false);
+    }
+
+}
