2c58d45fdbaad2fddb2faaff67222d2b41493381
Varun Patil <radialapps@gmail.com>
2019-02-02 02:12:29 +0530

    Fix messed up NotificationId

--- app/src/main/java/app/insti/notifications/NotificationId.java
+++ app/src/main/java/app/insti/notifications/NotificationId.java
@@ -4,23 +4,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class NotificationId {
     private final static AtomicInteger c = new AtomicInteger(0);
+    private final static AtomicInteger current_count = new AtomicInteger(0);
+
     public static int getID() {
         return c.incrementAndGet();
     }
 
-    private final static AtomicInteger current_count = new AtomicInteger(0);
     public static int getCurrentCount() {
-        return c.get();
+        return current_count.get();
     }
 
     public static int decrementAndGetCurrentCount() {
-        if (c.get() > 0)
-            return c.decrementAndGet();
+        if (current_count.get() > 0)
+            return current_count.decrementAndGet();
         else
             return 0;
     }
 
     public static void setCurrentCount(int count) {
-        c.set(count);
+        current_count.set(count);
     }
 }
